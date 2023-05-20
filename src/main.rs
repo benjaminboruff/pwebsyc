b3f05475271524152a7b515e8ad7aaab604d788a
Benjamin H Boruff <11185240+benjaminboruff@users.noreply.github.com>
2023-05-20 10:05:03 -0400

    basic contact plus tweeks elsewhere

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use components::card::Card;
+use components::contact::Contact;
 use components::hero::Hero;
 use sycamore::prelude::*;
 use sycamore_router::{HistoryIntegration, Route, Router};
@@ -11,6 +12,8 @@ enum AppRoutes {
     Index,
     #[to("/about")]
     About,
+    #[to("/contact")]
+    Contact,
     #[not_found]
     NotFound,
 }
@@ -21,33 +24,38 @@ fn main() {
             Router(
                 integration=HistoryIntegration::new(),
                 view=|cx, route: &ReadSignal<AppRoutes>| {
-                    let count = create_signal(cx, vec![1,2,3,4]);
+                    let count = create_signal(cx, vec![1,2,3,4]); // placeholder for project data
                     view! { cx,
                         div(class="app min-h-screen bg-sky-400") {
-                            div(class="content text-gray-900 font-sans") {
+                            div(class="text-gray-900 font-sans") {
                                 Hero{}
                                 div(class="container p-4 mx-auto"){
-                                        (match route.get().as_ref() {
-                                            AppRoutes::Index => view! {cx,
-                                                    Keyed(
-                                                        iterable=count,
-                                                        view=|cx, x| view! { cx,
-                                                            Card(item=x){}
-                                                        },
-                                                        key=|x| *x,
-                                                    )
-                                            },
-                                            AppRoutes::About => view!{cx,
-                                                article(class="flex flex-col justify-center items-center") {
-                                                    "This is the About page."
-                                                }
-                                            },
-                                            AppRoutes::NotFound => view! {cx,
-                                                "Well, you know, man, like whatever it is you are looking for ain't here. Dave's not here either."
+                                    (match route.get().as_ref() {
+                                        AppRoutes::Index => view! {cx,
+                                                Keyed(
+                                                    iterable=count,
+                                                    view=|cx, x| view! { cx,
+                                                        Card(item=x){}
+                                                    },
+                                                    key=|x| *x,
+                                                )
+                                        },
+                                        AppRoutes::About => view!{cx,
+                                            div(class="flex flex-col justify-center items-center") {
+                                                p(class="text-lg leading-8 text-gray-700") {"This is the About page."}
                                             }
-                                        })
+                                        },
+                                        AppRoutes::Contact => view!{cx,
+                                            Contact{}
+                                        },
+                                        AppRoutes::NotFound => view! {cx,
+                                            div(class="flex flex-col justify-center items-center") {
+                                                p (class="text-lg leading-8 text-gray-700"){"Well, you know, man, like whatever it is you are looking for ain't here."}
+                                                p (class="text-lg leading-8 text-gray-700"){"Dave's not here, either."}
+                                            }
+                                        }
+                                    })
                                 }
-
                             }
                         }
 
