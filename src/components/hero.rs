d69fad3ad99cc24d65bb0478b4a7d023c6de702a
Benjamin H Boruff <11185240+benjaminboruff@users.noreply.github.com>
2023-05-21 01:03:08 -0400

    tough initial tab state change functionality

--- src/components/hero.rs
+++ src/components/hero.rs
@@ -4,7 +4,7 @@ use sycamore::prelude::*;
 pub fn Hero<G: Html>(cx: Scope) -> View<G> {
     view! {cx,
         div( class="bg-sky-50 py-24 sm:py-32") {
-            div(class="mx-auto max-w-7xl px-6 lg:px-8") {
+            div(class="mx-auto max-w-3xl px-6 lg:px-3") {
                 div(class="mx-auto max-w-2xl lg:mx-0") {
                     img(class="inline-block h-14 w-14 rounded-md", src="/static/images/me.jpeg", alt="")
                     p(class="mt-6 text-lg leading-8 text-gray-600"){"Hey, I'm Ben Boruff, programmer and Linux/FreeBSD system administrator."}
